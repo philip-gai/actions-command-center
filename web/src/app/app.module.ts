fa814518005ab86cea9e9b33f4f0dd071e8dabd3
Philip Gai <philip-gai@github.com>
2023-03-29 17:35:42 +0000

    Create user input form

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,18 +5,33 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthFormComponent } from './auth-form/auth-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    AuthFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule
+    // Material Modules
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ web/src/app/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private readonly usernameKey = 'user.username';
+  private readonly tokenKey = 'user.token';
+
+  private _Username?: string | null | undefined;
+  public get Username(): string | null | undefined {
+    return localStorage.getItem(this.usernameKey) || this._Username;
+  }
+  public set Username(value: string | null | undefined) {
+    this._Username = value;
+    localStorage.setItem(this.usernameKey, value ?? '');
+  }
+
+  private _Token?: string | null | undefined;
+  public get Token(): string | null | undefined {
+    return localStorage.getItem(this.tokenKey) || this._Token;
+  }
+  public set Token(value: string | null | undefined) {
+    this._Token = value;
+    localStorage.setItem(this.tokenKey, value ?? '');
+  }
+
+  public ClearUser(): void {
+    this.Username = null;
+    this.Token = null;
+    localStorage.removeItem(this.usernameKey);
+    localStorage.removeItem(this.tokenKey);
+  }
+}
