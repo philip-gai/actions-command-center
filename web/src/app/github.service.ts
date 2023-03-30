eef2c1138275476df446a8d1debfcce2a6a9db98
Philip Gai <philip-gai@github.com>
2023-03-30 03:41:18 +0000

    Lint

--- web/src/app/github.service.ts
+++ web/src/app/github.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { isEmpty } from 'lodash';
 import { Octokit } from 'octokit';
-import { catchError, from, map, Observable, of } from 'rxjs';
+import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
