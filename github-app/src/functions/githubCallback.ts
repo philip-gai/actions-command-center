994c9bb2ba8954cb6188396b6a1616367c5de525
Philip Gai <philip-gai@github.com>
2023-04-08 19:04:05 +0000

    Init github callback Azure function

---
+++ github-app/src/functions/githubCallback.ts
@@ -0,0 +1,16 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+
+export async function githubCallback(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get('name') || await request.text() || 'world';
+
+    return { body: `Hello, ${name}!` };
+};
+
+app.http('githubCallback', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: githubCallback,
+    route: 'github/callback'
+});
