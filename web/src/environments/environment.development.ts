e8ee890d21b1eac7cb1397541ca76f71911f5bc2
Philip Gai <philip-gai@github.com>
2023-04-13 05:54:21 +0000

    Add githubAppClientId and githubRedirectUri

--- web/src/environments/environment.development.ts
+++ web/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:7071'
+  apiBaseUrl: 'http://localhost:7071',
+  githubAppClientId: 'Iv1.f3cd5e26efa9f14f',
+  githubRedirectUri: 'http://localhost:4200/github/callback'
 };
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://actions-command-center.azurewebsites.net'
+  apiBaseUrl: 'https://actions-command-center.azurewebsites.net',
+  githubAppClientId: 'Iv1.f3cd5e26efa9f14f',
+  githubRedirectUri: ''
 };
