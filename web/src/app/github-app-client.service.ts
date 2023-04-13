06a2054966995a6f0adff69ede219218b1eed724
Philip Gai <philip-gai@github.com>
2023-04-13 05:06:57 +0000

    Setup environment.apiBaseUrl

--- web/src/app/github-app-client.service.ts
+++ web/src/app/github-app-client.service.ts
@@ -1,15 +1,17 @@
 import { GitHubAppUserAuthenticationWithExpiration } from "@octokit/auth-oauth-app";
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubAppClientService {
+  accessTokenEndpoint = '/api/github/login/oauth/access_token';
 
   constructor(private _httpClient: HttpClient) { }
 
   public getUserAccessToken(code: string) {
-    return this._httpClient.get<GitHubAppUserAuthenticationWithExpiration>('http://localhost:7071/api/github/login/oauth/access_token', { params: { code } });
+    return this._httpClient.get<GitHubAppUserAuthenticationWithExpiration>(`${environment.apiBaseUrl}${this.accessTokenEndpoint}`, { params: { code } });
   }
 }
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  apiBaseUrl: 'https://actions-command-center.azurewebsites.net'
 };
