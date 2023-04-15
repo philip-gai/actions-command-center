68e75857e4d87d4249fb56c2a2813f269bde1696
Philip Gai <philip-gai@github.com>
2023-04-15 04:49:23 +0000

    Fix azure functions core tools

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -4,9 +4,4 @@ set -e
 
 # Install additional packages
 
-npm i -g install azure-functions-core-tools@4 --unsafe-perm true
-func --version || true
-
-cd web
-ng completion || true
-cd - > /dev/null
+source <(ng completion script)
