3d840d56c5144ca77d1fad9f387067b0f6d54cfb
Philip Gai <philip-gai@github.com>
2023-04-15 07:24:47 +0000

    Fix finding workflow run

--- api/src/functions/githubWebhooks.ts
+++ api/src/functions/githubWebhooks.ts
@@ -11,39 +11,42 @@ export async function githubWebhooks(request: HttpRequest, context: InvocationCo
     context.log(payload);
 
     if (event === "workflow_dispatch") {
-        context.log(`Received workflow_dispatch event`);
-
         const startTime = new Date();
         context.log(`Start time: ${startTime}`);
-        
+
         const appId = env["GITHUB_APP_ID"];
         const appPk = env["GITHUB_APP_PRIVATE_KEY"];
-    
+
         if (!appId || !appPk) {
             context.error(`Missing GITHUB_APP_ID or GITHUB_APP_PRIVATE_KEY`);
             return { status: 500, body: "Sorry, looks like we're running into some issues. Try again later." };
         }
-    
+
         const app = new App({ appId, privateKey: appPk });
 
         const installKit = await app.getInstallationOctokit(payload.installation.id)
 
-        const workflowRuns = await installKit.rest.actions.listWorkflowRunsForRepo({
+        // Wait a few seconds
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        const workflowRunsResponse = await installKit.rest.actions.listWorkflowRunsForRepo({
             owner: payload.repository.owner.login,
             repo: payload.repository.name,
             status: 'waiting',
             event: 'workflow_dispatch',
             actor: payload.sender.login
         })
+        console.log(workflowRunsResponse);
+        const workflowRuns = workflowRunsResponse.data.workflow_runs;
         context.log(workflowRuns);
-        const matches = workflowRuns.data.workflow_runs.filter(run => {
-            const runStarted = Date.parse(run.run_started_at);
-            context.log(`Run started at ${runStarted}`);
+        const matches = workflowRuns.filter(run => {
+            const runStarted = new Date(Date.parse(run.run_started_at));
+            context.log(`Checking run started at ${runStarted}`);
             return run.workflow_url.endsWith(payload.workflow)
-            // Started really close (within seconds) to the webhook being sent
-            && runStarted >= startTime.getTime() - 1000
+                // Started really close (within seconds) to the webhook being sent
+                && runStarted.getTime() >= startTime.getTime() - 1000
         });
-        if(matches.length === 0) {
+        if (matches.length === 0) {
             context.log(`No matching workflow runs found`);
             return { status: 200, body: 'OK' };
         } else {
